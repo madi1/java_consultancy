d2f2318654d5a1a3e19aca41e00b5f76472f1a5b
madi1 <madi1@users.noreply.github.com>
2026-03-10 03:03:06 +0300

    Delete microservices/src/main/java/com/madi/config directory

--- microservices/src/main/java/com/madi/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // Change this to your Azure URL later
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
-    }
-}
